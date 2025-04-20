3e2c3016d2ecd04db139f8671aff795b5fc97634
jpascua123 <jpascua@torontomu.ca>
2025-04-20 16:38:06 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('uv-form');
+const input = document.getElementById('uv-search-engine');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  let url = input.value;
+  if (!url.startsWith('http')) url = 'https://' + url;
+  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
